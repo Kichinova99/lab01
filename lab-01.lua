926b34efd50c3601baed5a456705c123cd632460
Kichinova99 <alyonakichinova@mail.ru>
2019-11-07 18:34:44 +0300

    added lab-01.lua

---
+++ lab-01.lua
@@ -0,0 +1,13 @@
+io.write('Vvedite 2 chisla: ')
+a = io.read('*n')
+b = io.read('*n')
+
+sum = a + b
+sub = a - b
+mul = a * b
+del = a / b
+
+print('Slozhenie: ', sum)
+print('Vichitanie: ', sub)
+print('Umnozhenie: ', mul)
+print('Delenie: ', del)
